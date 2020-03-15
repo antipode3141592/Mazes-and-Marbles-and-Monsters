5bd571fdcb69cdf1be132766f4ba2664e2e11054
Sean <seanvokirkpatrick@gmail.com>
2020-03-15 06:21:37 -0700

    moved prefabs around, updated torches (lit and unlit)

--- Assets/Scripts/Torch.cs
+++ Assets/Scripts/Torch.cs
@@ -5,6 +5,9 @@ using UnityEngine.Experimental.Rendering.Universal;
 
 public class Torch : MonoBehaviour
 {
+    [SerializeField]
+    private bool initialState = false;
+
     private Light2D lightObject;
     private Animator animationController;
 
@@ -14,11 +17,19 @@ public class Torch : MonoBehaviour
         animationController = gameObject.GetComponent<Animator>();
     }
 
+    private void Start()
+    {
+        if (initialState)
+        {
+            animationController.SetBool("initialState", true);
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other != null && other.gameObject.CompareTag("Player"))
         {
-            lightObject.intensity = 1.0f;
+            //lightObject.intensity = 1.0f;
             animationController.SetTrigger("Light");
         }
         
